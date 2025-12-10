0c069040e377baf1b66a5d5b2e901d4cb39ab72e
Vijeta Yadav <yadavvijeta878@gmail.com>
2025-12-10 23:37:21 +0530

    ExcelSheet Que

---
+++ src/com/pedia/ExcelSheet.java
@@ -0,0 +1,23 @@
+package com.pedia;
+
+public class ExcelSheet {
+	public static void main(String[] args) {
+
+//class Solution {
+//    public String convertToTitle(int columnNumber) {
+//    	StringBuilder sb=new StringBuilder();
+//    	
+//    	while(columnNumber>0) {
+//    		columnNumber--;
+//    		int rem=columnNumber%26;
+//    		char ch=(char)(rem+'A');
+//    		sb.append(ch);
+//    		columnNumber=columnNumber/26;
+//    	}
+//    	return sb.reverse().toString();
+//    	}
+//    }
+//		
+//		
+	}
+}
