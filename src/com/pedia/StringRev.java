73a397d940958ec3a0375e171d40e8ba1a6c8f72
Vijeta Yadav <yadavvijeta878@gmail.com>
2025-12-15 00:00:10 +0530

    practice

---
+++ src/com/pedia/StringRev.java
@@ -0,0 +1,22 @@
+package com.pedia;
+
+public class StringRev {
+	public static void main(String[] args) {
+		String str="Vijeta";
+		String s=str.toLowerCase();
+		String rev="";
+		
+		for(int i=s.length()-1;i>=0;i--) {
+			rev+=s.charAt(i);
+		}
+		System.out.println(rev);
+		
+		if(s.equals(rev)) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not a Palindrome");
+		}
+	}
+
+}
